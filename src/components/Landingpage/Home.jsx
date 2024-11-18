15273efe56478a42c3c2f78e00e959f4324cf914
Mark Bandola <143799613+MarkJanzenB@users.noreply.github.com>
2024-11-18 15:09:20 +0800

    11/18/2024 -removed background image replaced with citu logo pero wala pa nigawas ang logo :<

--- src/components/Landingpage/Home.jsx
+++ src/components/Landingpage/Home.jsx
@@ -4,20 +4,25 @@ import './LandingPage.css';
 
 const LandingPage = () => {
   return (
-    <div id='wrapper'>
-        <Appbar page={'home'}/>
-       <br/> <br/>
-       <div className="landing-page">
-       <div className="content">
-       <h1>Laboratory Equipment</h1>
-       <h1>Monitoring System</h1>
-       <p>Optimize lab operations with our app for efficient equipment borrowing, breakage assessment, and real-time inventory management.</p>
-     </div>
-     <div id="imgcontainer">
-        <img src="../src/assets/static/img/scientistnobg.png" />
-     </div> 
-    </div>
-    </div>
+      <div id='wrapper'>
+          <Appbar page={'home'}/>
+          <div className="waterMark"></div>
+
+          <br/> <br/>
+          <div className="landing-page">
+              <div className="content">
+                  <h1>Laboratory Equipment</h1>
+                  <h1>Monitoring System</h1>
+                  <p>Optimize lab operations with our app for efficient equipment borrowing, breakage assessment, and
+                      real-time inventory management.</p>
+              </div>
+
+              <div id="imgcontainer">
+                  <img src="src/assets/static/img/scientistnobg.png"/>
+              </div>
+          </div>
+
+      </div>
   );
 };
 
