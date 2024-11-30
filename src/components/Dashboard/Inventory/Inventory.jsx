d4c0898178d03d3694fc593ee7a22fcabac2e5a4
NifaNii <jcarlsabejon@gmail.com>
2024-12-01 03:32:27 +0800

    Added add function (can only add new items)

--- src/components/Dashboard/Inventory/Inventory.jsx
+++ src/components/Dashboard/Inventory/Inventory.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useState } from "react";
 import Appbar from "../../Appbar/Appbar.jsx";
 // import "./Sidebar/Sidebar.css";
 import {
     Button,
     Modal,
-    Box, Typography, TextField, ButtonGroup
+    Box, Typography, TextField, ButtonGroup,
+    Select,
+    MenuItem
 } from "@mui/material";
 import Sidebar from "../../Sidebar/Sidebar.jsx";
 import MyPaper from "../../MyPaper.jsx";
@@ -23,6 +25,8 @@ const columns = [
 
 
 export default function Inventory() {
+    const jwtToken = localStorage.getItem("jwtToken");
+    const [error, setError] = useState('');
     const [showTable, setShowTable] = useState(false);
     const [data, setData] = useState([{}]);
     const [page, setPage] = useState(0);
@@ -30,24 +34,37 @@ export default function Inventory() {
     const [currentCategory, setCurrentCategory] = useState(0);
     const [transition, setTransition] = useState(false);
     const [openModal, setOpenModal] = useState(false);
+    const [currentStep, setCurrentStep] = useState(1);
+    const [newItemCategory, setNewItemCategory] = useState(1);
+    const newInvId = useRef(0);
     const [newItem, setNewItem] = useState({
-        id: '',
-        serialNum: '',
-        name: '',
-        description: '',
-        stock: '',
-        status: ''
+        item_name: '',
+        unique_id: '',
+        group_id: null,
+        inventory: {inventory_id:0}
     });
+    const [newConsumable, setNewConsumable] = useState({
+        unit: '',
+        name: '',
+        item_category:{category_id:0},
+        quantity:0,
+        description:'',
+    })
 
     const handleModalClose = () => {
         setOpenModal(false);
+        setCurrentStep(1);
+        setError('');
+    }
+
+    const handleModalBack = () => {
+        setCurrentStep(1);
     }
 
     const handleViewListClick = (categoryId) => {
         setTransition(true);
         setTimeout(async () => {
             setCurrentCategory(categoryId);
-            const jwtToken = localStorage.getItem("jwtToken");
 
             console.log("Inventory JWT: ", jwtToken);
             console.log("Inventory Request: http://localhost:8080/inventory/getinventorybycategory?categoryId=" + categoryId+1);
@@ -68,7 +85,6 @@ export default function Inventory() {
         setTransition(true);
         setTimeout(async () => {
             setCurrentCategory(categoryId);
-            const jwtToken = localStorage.getItem("jwtToken");
 
             const response = await axios.get("http://localhost:8080/inventory/getAllInventory", {
                 headers: {
@@ -113,20 +129,138 @@ export default function Inventory() {
         setOpenModal(true);
     };
 
-    const handleInputChange = (e) => {
+    const handleInputChangeCategory = (e) => {
         const { name, value } = e.target;
-        setNewItem(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
+        if(name == "category"){
+            setNewItemCategory(value);
+        }
+        
     };
 
+    const handleInputChange = (e) => {
+        const { name, value} = e.target;
+        setError('');
+
+        if(newItemCategory != 1){
+            setNewItem(prevState => ({
+                ...prevState,
+                [name]: value
+            }));
+        }else{
+            setNewConsumable(prevState => ({
+                ...prevState,
+                [name]: value
+            }));
+        }
+    }
+
+    const handleNext = () => {
+        setCurrentStep(2);
+
+        if(newItemCategory == 1){
+            setNewConsumable(prevState => ({
+                ...prevState,
+                item_category: {category_id: newItemCategory}
+            }));
+        }
+    }
+
     const handleAddItem = () => {
-        const updatedData = [...data, { ...newItem, id: data.length + 1 }];
-        setData(updatedData);
-        categoryDataMap[categories[currentCategory]] = updatedData;
-        categoryDataMap['All items'] = [...categoryDataMap['All items'], { ...newItem, id: categoryDataMap['All items'].length + 1 }];
-        setOpenModal(false);
+        if(newItemCategory == 1){
+            console.log(newConsumable)
+            console.log(newItemCategory)
+            axios.post("http://localhost:8080/inventory/addinventory", newConsumable, {
+                headers: {
+                    "Authorization": `Bearer ${jwtToken}`
+                }
+            })
+            .then(response => {
+                setOpenModal(false);
+                setCurrentStep(1);
+            })
+            .catch(error => {
+                if(error.response.status == 409){
+                    setError("Item already exists. Did you mean add stock?");
+                }else{
+                    console.log(error);
+                    setError("An unexpected error occured.");
+                }
+            })
+            
+        }else{
+            axios.get(`http://localhost:8080/inventory/isinventoryexists?inventoryName=${newItem.item_name}`, {
+                headers: {
+                    "Authorization": `Bearer ${jwtToken}`
+                }
+            })
+            .then(response => {
+                setError("Item already exists. Did you mean add stock?");
+            })
+            .catch(error => {
+                if(error.response.status == 409){
+                    axios.post("http://localhost:8080/inventory/addinventory", {
+                        unit: null,
+                        name: newItem.item_name,
+                        item_category:{
+                            category_id:newItemCategory
+                        },
+                        quantity: 1
+                    },{
+                        headers:{
+                            "Authorization": `Bearer ${jwtToken}`
+                        }
+                    })
+                    .then(response => { 
+                        newInvId.current = response.data.inventory_id;
+                        axios.post("http://localhost:8080/item/insertitem", {
+                            item_name: newItem.item_name,
+                            unique_id: newItem.unique_id,
+                            group_id: newItem.group_id,
+                            inventory: {
+                                inventory_id: response.data.inventory_id //we need to get this
+                            }
+                        },{
+                            headers:{
+                                "Authorization": `Bearer ${jwtToken}`
+                            }
+                        })
+                        .then(response => {
+                            console.log("insert item resposne: ", response);
+                            setOpenModal(false);
+                            setCurrentStep(1);
+                        })
+                        .catch(error => {
+                            console.log(error.response);
+                            if(error.response.status == 409){
+                                setError(error.response.data);
+                                console.log("inventory ID when error: ",newInvId.current);
+                                axios.delete(`http://localhost:8080/inventory/delete/${newInvId.current}`, {
+                                    headers:{
+                                        "Authorization": `Bearer ${jwtToken}`
+                                    }
+                                })
+                                .then(response => {
+                                    console.log("delete inventory response: ", response);
+                                })
+                                .catch(error => {
+                                    console.log("delete inventory error: ", error.response);
+                                })
+                            }else{
+                                console.log(error);
+                                setError("An unexpected error occured.");
+                            }
+                        })
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        setError("An unexpected error occured.");
+                    })
+                }else{
+                    console.log(error);
+                    setError("An unexpected error occured.");
+                }
+            })
+        }
     };
 
     const paginatedData = data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
@@ -246,34 +380,123 @@ export default function Inventory() {
                             )}
                         </MyPaper>
                         <Modal open={openModal} onClose={handleModalClose}>
-                            <Box
-                                sx={{
-                                    position: 'absolute',
-                                    top: '50%',
-                                    left: '50%',
-                                    transform: 'translate(-50%, -50%)',
-                                    width: 500,
-                                    bgcolor: '#F2EE9D',
-                                    boxShadow: 24,
-                                    p: 4,
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    gap: 2,
-                                    borderRadius: '25px'
-                                }}
-                            >
-                                <Typography variant={'h6'} component={'div'} sx={{ fontWeight: 'bold', color: '#016565', textAlign: 'center' }}>
-                                    ADD ITEM
-                                </Typography>
-                                <TextField name="serialNum" value={newItem.serialNum} onChange={handleInputChange} sx={{ backgroundColor: '#FFFFFF', borderRadius: '10px' }} label="Serial Number" variant="outlined" fullWidth />
-                                <TextField name="name" value={newItem.name} onChange={handleInputChange} sx={{ backgroundColor: '#FFFFFF', borderRadius: '10px' }} label="Name" variant="outlined" fullWidth />
-                                <TextField name="description" value={newItem.description} onChange={handleInputChange} sx={{ backgroundColor: '#FFFFFF', borderRadius: '10px' }} label="Description" variant="outlined" fullWidth />
-                                <TextField name="stock" value={newItem.stock} onChange={handleInputChange} sx={{ backgroundColor: '#FFFFFF', borderRadius: '10px' }} label="Stock" variant="outlined" fullWidth />
-                                <TextField name="status" value={newItem.status} onChange={handleInputChange} sx={{ backgroundColor: '#FFFFFF', borderRadius: '10px' }} label="Status" variant="outlined" fullWidth />
-                                <Box display="flex" justifyContent="space-between" mt={2}>
-                                    <Button variant="outlined" sx={{ color: '#800000', borderColor: '#800000' }} onClick={handleModalClose}>Close</Button>
-                                    <Button variant="contained" sx={{ backgroundColor: '#800000', color: '#FFF', '&:hover': { backgroundColor: '#5c0000' } }} onClick={handleAddItem}>AddItem</Button>
-                                </Box>
+                            <Box>
+                                {currentStep === 1 && (
+                                    <Box
+                                        sx={{
+                                            position: 'absolute',
+                                            top: '50%',
+                                            left: '50%',
+                                            transform: 'translate(-50%, -50%)',
+                                            width: 500,
+                                            bgcolor: '#F2EE9D',
+                                            boxShadow: 24,
+                                            p: 4,
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            gap: 2,
+                                            borderRadius: '25px',
+                                        }}
+                                    >
+                                        <Typography variant="h6" component="div" sx={{ fontWeight: 'bold', color: '#016565', textAlign: 'center' }}>
+                                            SELECT CATEGORY
+                                        </Typography>
+                                        <Select
+                                            name="category"
+                                            value={newItemCategory}
+                                            onChange={handleInputChangeCategory}
+                                            sx={{ color: '#000' }}
+                                        >
+                                            <MenuItem value={1}>Consumables</MenuItem>
+                                            <MenuItem value={2}>Equipment</MenuItem>
+                                            <MenuItem value={3}>Glassware</MenuItem>
+                                            <MenuItem value={4}>Hazards</MenuItem>
+                                        </Select>
+                                        <Button
+                                            variant="contained"
+                                            sx={{ backgroundColor: '#800000', color: '#FFF', '&:hover': { backgroundColor: '#5c0000' } }}
+                                            onClick={handleNext}
+                                        >
+                                            Next
+                                        </Button>
+                                    </Box>
+                                )}
+
+                                {currentStep === 2 && (
+                                    <Box
+                                        sx={{
+                                            position: 'absolute',
+                                            top: '50%',
+                                            left: '50%',
+                                            transform: 'translate(-50%, -50%)',
+                                            width: 500,
+                                            bgcolor: '#F2EE9D',
+                                            boxShadow: 24,
+                                            p: 4,
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            gap: 2,
+                                            borderRadius: '25px',
+                                        }}
+                                    >
+                                        <Typography variant="h6" component="div" sx={{ fontWeight: 'bold', color: '#016565', textAlign: 'center' }}>
+                                            ADD ITEM
+                                        </Typography>
+                                        {newItemCategory != 1 && (
+                                            <TextField
+                                            name="unique_id"
+                                            value={newItem.serialNum}
+                                            onChange={handleInputChange}
+                                            sx={{ backgroundColor: '#FFFFFF', borderRadius: '10px' }}
+                                            label="Serial Number"
+                                            variant="outlined"
+                                            fullWidth
+                                            />
+                                        )}
+                                        <TextField
+                                            name={newItemCategory !== 1 ? 'item_name' : 'name'}
+                                            value={newItemCategory !== 1 ? newItem.item_name : newItem.name}
+                                            onChange={handleInputChange}
+                                            sx={{ backgroundColor: '#FFFFFF', borderRadius: '10px' }}
+                                            label="Name"
+                                            variant="outlined"
+                                            fullWidth
+                                        />
+                                        <TextField
+                                            name="description"
+                                            value={newItem.description}
+                                            onChange={handleInputChange}
+                                            sx={{ backgroundColor: '#FFFFFF', borderRadius: '10px' }}
+                                            label="Description"
+                                            variant="outlined"
+                                            fullWidth
+                                        />
+                                        {newItemCategory == 1 && (
+                                            <TextField
+                                            name="quantity"
+                                            value={newItem.stock}
+                                            onChange={handleInputChange}
+                                            sx={{ backgroundColor: '#FFFFFF', borderRadius: '10px' }}
+                                            label="Quantity"
+                                            variant="outlined"
+                                            fullWidth
+                                            />
+                                        )}
+                                        {error && <Typography color="error" sx={{mt:2}}>{error}</Typography>}
+                                        <Box display="flex" justifyContent="space-between" mt={2}>
+                                            <Button variant="outlined" sx={{ color: '#800000', borderColor: '#800000' }} onClick={handleModalBack}>
+                                                Back
+                                            </Button>
+                                            <Button
+                                                variant="contained"
+                                                sx={{ backgroundColor: '#800000', color: '#FFF', '&:hover': { backgroundColor: '#5c0000' } }}
+                                                onClick={handleAddItem}
+                                            >
+                                                Add Item
+                                            </Button>
+                                        </Box>
+                                    </Box>
+                                )}
                             </Box>
                         </Modal>
                     </div>
