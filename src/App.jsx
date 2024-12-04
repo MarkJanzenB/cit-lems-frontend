649f77a5ec5681e6bb5cfb39045a50111a67bcc5
Mark Bandola <143799613+MarkJanzenB@users.noreply.github.com>
2024-12-04 14:27:15 +0800

    12/4/2024 Mark Janzen Bandola - Added Page title "LEMS" for the whole site

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './components/Routes.jsx';
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 
 function App() {
   return (
         <div className='MainBG'>
             <BrowserRouter>
+                <Helmet>
+                    <title>LEMS</title>
+                </Helmet>
                 <Routes />
             </BrowserRouter>
         </div>
